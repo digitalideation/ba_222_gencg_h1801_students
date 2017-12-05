5d921076c5529267cad7e80c3149b2b0d3168d47
guillaume <4388440+gu-ma@users.noreply.github.com>
2017-12-05 22:33:16 +0100

    added sketch for homepage

--- sketch.js
+++ sketch.js
@@ -1,63 +1,84 @@
-// Based on the code P_2_1_1_01.pde from
-// Generative Gestaltung, ISBN: 978-3-87439-759-9
+// Noise generated circle
 
 // Global var
-var tileCount, actRandomSeed, actStrokeCap;
- 
+// Some of the var might be initialised in gui.js
+var canvas, backgroundGrey, radius;
+var actRandomSeed, count, points, increment;
+
 function setup() {
   // Canvas setup
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5Container");
   // Detect screen density (retina)
+  // Comment it out if the sketch is too slow
   var density = displayDensity();
   pixelDensity(density);
-  // Colors and drawing modes
-  smooth();
-  // Init Var
-  tileCount = 20;
-  actRandomSeed = 0;
-  actStrokeCap = ROUND; 
+  // Init var
+  // some of the var are initialised in gui.js
+  backgroundGrey = 0;
+  count = 150;
+  points = [count];
+  background(backgroundGrey);
+  radius = 20;
+  increment = +1;
 }
 
 function draw() {
-  // Canvas draw options
-  background(255);
+  // background(backgroundGrey, 20);
   smooth();
-  noFill();
 
-  // Stroke options
-  strokeCap(actStrokeCap);
-  randomSeed(actRandomSeed);
+  // Create points array
+  let faderX = .1;
+  let t = millis()/1000;
+  // let r = map(mouseY,0,height,10,radius);
+  if (radius>width/1.7 && radius>height/1.7) increment = -increment;
+  else if (radius<20) increment = -increment;
+  radius += increment; 
+  let angle = radians(360/count);
 
-  for (let gridX = 0; gridX < tileCount; gridX++) {
-    for (let gridY = 0; gridY < tileCount; gridY++) {
-
-      let posX = width / tileCount * gridX;
-      let posY = width / tileCount * gridY;
-
-      let toggle = toInt(random(0, 2));
-
-      if (toggle == 0) {
-        strokeWeight(mouseX / 20);
-        line(posX, posY, posX + width / tileCount, posY + height / tileCount);
-
-      } else if (toggle == 1) {
-        strokeWeight(mouseY / 20);
-        line(posX, posY + width / tileCount, posX + height / tileCount, posY);
-      }
-    }
+  for (let i=0; i<count; i++){
+    let radiusRand = radius - noise(t, i*faderX)*50;
+    let x = width/2 + cos(angle*i)*radiusRand;
+    let y = height/2 + sin(angle*i)*radiusRand;
+    points[i] = createVector(x,y);
   }
+
+  // Draw
+  // stroke(noise(t/10)*255,0,noise(t/1)*100,255);
+  strokeHsluv(noise(t/10)*360,noise(t/20)*50,noise(t)*80);
+  strokeWeight(20);
+  noFill();
+  beginShape();
+  for (let i=0; i<count; i++){
+    // fill(255);
+    // ellipse(points[i].x, points[i].y,2,2);
+    // noFill();
+    curveVertex(points[i].x, points[i].y);
+    if (i==0 || i==count-1) curveVertex(points[i].x, points[i].y);
+  }
+  endShape(CLOSE);
 }
 
-function mousePressed() {
-  actRandomSeed = toInt(random(100000));
-}
 
 function keyPressed() {
+  if (key == DELETE || key == BACKSPACE) background(360);  
   if (key == 's' || key == 'S') saveThumb(650, 350);
-  if (key == '1') actStrokeCap = ROUND;
-  if (key == '2') actStrokeCap = SQUARE;
-  if (key == '3') actStrokeCap = PROJECT;
+}
+
+// Color functions
+function fillHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  fill(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+function strokeHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  stroke(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
+}
+
+function colorHsluv(h, s, l) {
+  var rgb = hsluv.hsluvToRgb([h, s, l]);
+  return color(rgb[0] * 255, rgb[1] * 255, rgb[2] * 255);
 }
 
 // Tools
@@ -67,7 +88,7 @@ function windowResized() {
   resizeCanvas(windowWidth, windowHeight, false);
 }
 
-// Int conversion
+//  conversion
 function toInt(value) {
   return ~~value;
 }
@@ -79,6 +100,6 @@ function timestamp() {
 
 // Thumb
 function saveThumb(w, h) {
-  let img = get( width/2-w/2, height/2-h/2, w, h);
-  save(img,'thumb.jpg');
+  let img = get(width / 2 - w / 2, height / 2 - h / 2, w, h);
+  save(img, 'thumb.jpg');
 }
